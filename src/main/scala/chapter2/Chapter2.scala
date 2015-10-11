eb2ee6aa66c1d37dce6ddd1fc4d3bba879ada085
konrad <k.wudkowski@accenture.com>
2015-10-11 23:01:51 +0100

    finished exercises from ch2

--- src/main/scala/chapter2/Chapter2.scala
+++ src/main/scala/chapter2/Chapter2.scala
@@ -1,3 +1,4 @@
+
 package chapter2
 
 import scala.annotation.tailrec
@@ -11,5 +12,28 @@ object Chapter2 {
       }
       if (n != 0 ) go(0,1,1) else 0
   }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @tailrec
+    def go(ctr: Int): Boolean = {
+      if (ctr == as.length) {
+        true
+      } else {
+        if (ordered(as(ctr-1),as(ctr))) go(ctr + 1) else false
+      }
+    }
+    if (as.length == 0 || as.length == 1) true else go(1)
+  }
+
+  // alternative version without manual recursion
+  def isSorted2[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = as.sliding(2).forall {
+    case Array(first,second) => ordered(first,second)
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = a => b => f(a,b)
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = (a,b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 
 }
--- src/test/scala/chapter2/Chapter2Spec.scala
+++ src/test/scala/chapter2/Chapter2Spec.scala
@@ -11,4 +11,40 @@ class Chapter2Spec extends FreeSpec with Matchers {
     fib(6) shouldBe 8
     fib(11) shouldBe 89
   }
+
+  "function `isSorted` should" - {
+    "check whether array is sorted according to a given comparison function" in {
+      def orderedInt(x1: Int, x2: Int): Boolean = x1 <= x2
+      isSorted(Array(1,2,3,4,5,6), orderedInt) shouldBe true
+      isSorted(Array(1,3,3), orderedInt) shouldBe true
+      isSorted(Array(1,4,3), orderedInt) shouldBe false
+
+      def stringLengthOrdered(s1: String, s2: String) = s1.length <= s2.length
+      isSorted(Array("konrad","konrad1", "konrad123"), stringLengthOrdered) shouldBe true
+      isSorted(Array("konrad1","konrad1", "konrad123"), stringLengthOrdered) shouldBe true
+      isSorted(Array("konrad12","konrad1", "konrad123"), stringLengthOrdered) shouldBe false
+    }
+    "work for empty arrays" in {
+      def orderedInt(x1: Int, x2: Int): Boolean = true
+      isSorted[Int](Array(),orderedInt) shouldBe true
+    }
+  }
+
+  "function `isSorted2` should" - {
+    "check whether array is sorted according to a given comparison function" in {
+      def orderedInt(x1: Int, x2: Int): Boolean = x1 <= x2
+      isSorted2(Array(1,2,3,4,5,6), orderedInt) shouldBe true
+      isSorted2(Array(1,3,3), orderedInt) shouldBe true
+      isSorted2(Array(1,4,3), orderedInt) shouldBe false
+
+      def stringLengthOrdered(s1: String, s2: String) = s1.length <= s2.length
+      isSorted2(Array("konrad","konrad1", "konrad123"), stringLengthOrdered) shouldBe true
+      isSorted2(Array("konrad1","konrad1", "konrad123"), stringLengthOrdered) shouldBe true
+      isSorted2(Array("konrad12","konrad1", "konrad123"), stringLengthOrdered) shouldBe false
+    }
+    "work for empty arrays" in {
+      def orderedInt(x1: Int, x2: Int): Boolean = true
+      isSorted2[Int](Array(),orderedInt) shouldBe true
+    }
+  }
 }
